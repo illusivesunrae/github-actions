494b29bfd79dceb5ea6b03362ddfbe506ad39329
Rachel <racmocon@gmail.com>
2019-11-20 11:06:34 -0500

    Moved over base from rivet-add-on-boilerplate package

---
+++ src/js/rivet-actions.js
@@ -0,0 +1,15 @@
+export class AddOn {
+  constructor(options) {
+    this._handleClick = this._handleClick.bind(this);
+
+    this.init(options);
+  }
+
+  _handleClick() {
+    alert('Button clicked!');
+  }
+
+  init(options) {
+    document.getElementById(options.id).addEventListener('click', this._handleClick, false);
+  }
+}
